8faf34ba3d163b9eeaa1cc58b7f924d2633fdaa2
sardaruzair56 <116259618+sardaruzair56@users.noreply.github.com>
2023-11-10 18:37:39 +0500

    commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -12,35 +12,49 @@ const Header = () => {
   return (
     <div className={classes.body}>
       <div className={classes.header}>
-        <div>
-          <p className={classes["header-home"]}>Home</p>
-          <p className={classes["header-about"]}>About</p>
-          <p className={classes["header-contact"]}>Contact</p>
-          <p className={classes["header-landing"]}>Landing</p>
-          <button className={classes["header-button"]}>Buy Now</button>
-          <p className={classes.intro}>Introduce Your Product Quickly & Effectively</p>
-          <p className={classes.text}>
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-            <br/>
-            <br /> Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
-          </p>
-          <div className={classes.button}>
-            <button className={classes.button1}>Purchase UI kit</button>
-            <button className={classes.button2}>Learn More</button>
-          </div>
-          <div className={classes.menudiv}>
-            <button className={classes.menu} onClick={toggleDrawer}>☰ MENU</button>
-            {isDrawerOpen && (
-              <div className={classes.drawer}>
-                <ul>
-                  <li><a href="#">Home</a></li>
-                  <li><a href="#">About</a></li>
-                  <li><a href="#">Contact</a></li>
-                </ul>
-              </div>
-            )}
-          </div>
-        </div>
+        <p className={classes.home}>Home</p>
+        <p className={classes.about}>About</p>
+        <p className={classes.contact}>Contact</p>
+        <p className={classes.landing}>Landing</p>
+        <button className={classes.button}>Buy Now</button>
+      </div>
+      <p className={classes.groupImg} />
+      <div>
+        {" "}
+        <button className={classes.menubtn} onClick={toggleDrawer}>
+          ☰ MENU
+        </button>
+        {isDrawerOpen && (
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+            <li>
+              <button>Buy Now</button>
+            </li>
+          </ul>
+        )}
+      </div>
+      <div className={classes.txt}>
+        <p className={classes.intro}>
+          Introduce Your Product Quickly & Effectively
+        </p>
+        <p className={classes.text}>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+          commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus
+          et magnis dis parturient montes, nascetur ridiculus mus.
+          <br />
+          <br /> Donec quam felis, ultricies nec, pellentesque eu, pretium quis,
+          sem. Nulla consequat massa quis enim.
+        </p>
+        <button className={classes.button1}>Purchase UI kit</button>
+        <button className={classes.button2}>Learn More</button>
       </div>
       <LayoutDisplay />
     </div>
